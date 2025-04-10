bde7c7c285c4227db0166214628a44b8288e6df5
karantec <karanrana3095@gmail.com>
2025-04-10 19:21:48 +0530

    commit

--- app.js
+++ app.js
@@ -26,9 +26,16 @@ app.get('/', async (req, res, next) => {
   res.send({ message: 'Awesome it works 🐻' });
 });
 
-
-app.use(cors( ));
-
+app.options('*', cors()); 
+// OR Enable CORS with specific options
+app.use(
+  cors({
+    origin: ['https://jewellery-app-dashboard.vercel.app', 'http://localhost:3000','http://localhost:3001/','https://ecommerce-flax-chi.vercel.app/'], // Allow requests only from your dashboard
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],   // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Custom headers if required
+    credentials: true // Enable cookies or HTTP authentication headers
+  })
+);
 // app.use('/api', require('./routes/api.route'));
 app.use('/auth', UserRoutes);
 app.use('/gold',GoldRoutes);
