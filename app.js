6a8e9b5d4ec1019439f28e6d0ddd741565122909
karantec <karanrana3095@gmail.com>
2025-04-10 19:11:10 +0530

    commit

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.options('*', cors());
 // OR Enable CORS with specific options
 app.use(
   cors({
-    origin: ['https://jewellery-app-dashboard.vercel.app', 'http://localhost:3000','http://localhost:3001/','https://ecommerce-flax-chi.vercel.app/'], // Allow requests only from your dashboard
+    origin: ['https://jewellery-app-dashboard.vercel.app','https://ecommerce-flax-chi.vercel.app/'], // Allow requests only from your dashboard
     methods: ['GET', 'POST', 'PUT', 'DELETE'],   // Allowed HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Custom headers if required
     credentials: true // Enable cookies or HTTP authentication headers
