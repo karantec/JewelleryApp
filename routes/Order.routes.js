ffc28affc6b476e792e50ac059371af717293908
KARAN RANA <103360966+karantec@users.noreply.github.com>
2025-06-07 16:15:10 +0530

    Update Order.routes.js

--- routes/Order.routes.js
+++ routes/Order.routes.js
@@ -1,24 +1,25 @@
-// // routes/Order.routes.js
-// const express = require('express');
+// routes/Order.routes.js
+const express = require("express");
 
-// const { verifyToken, isAdmin } = require('../middleware/authmiddleware');
-// const { createOrder, getOrdersByUser, getOrderById, getAllOrders, verifyPayment } = require('../controller/Order.Controlller');
-// const router = express.Router();
+const { verifyToken, isAdmin } = require("../middleware/authmiddleware");
+const { createOrder } = require("../controller/Order.Controlller");
 
-// // 🔹 Create a new order (Authenticated users only)
-// router.post('/create', verifyToken, createOrder);
+const router = express.Router();
+
+// 🔹 Create a new order (Authenticated users only)
+router.post("/create", verifyToken, createOrder);
 
 // // 🔹 Get all orders for the logged-in user
-// router.get('/my-orders', verifyToken, getOrdersByUser);
+// router.get("/my-orders", verifyToken, getOrdersByUser);
 
 // // 🔹 Get a specific order by ID
-// router.get('/:id', verifyToken, getOrderById);
+// router.get("/:id", verifyToken, getOrderById);
 
-// router.post('/verify', verifyPayment);
-// // 🔹 Cancel an order
-// // router.put('/:id/cancel', verifyToken, cancelOrder);
+// router.post("/verify", verifyPayment);
+// 🔹 Cancel an order
+// router.put('/:id/cancel', verifyToken, cancelOrder);
 
 // // 🔹 Admin: Get all orders
-// router.get('/',getAllOrders);
+// router.get("/", getAllOrders);
 
-// module.exports = router;
+module.exports = router;
